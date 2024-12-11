35b75fd832417f6e7956b969a8c1a64dbcf074a4
Kalvin Garcia <kalvigarcia@gmail.com>
2024-12-11 15:01:59 -0500

    created overview form file

---
+++ src/components/overview-form.jsx
@@ -0,0 +1,11 @@
+import React, {} from 'react';
+import {createUseStyles} from 'react-jss';
+import { Subheading } from './common/typography';
+
+export default function OverviewForm() {
+    return (
+        <div>
+            <Subheading>Overview</Subheading>
+        </div>
+    );
+}
